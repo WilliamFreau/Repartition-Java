5d844716ef03048648054ad544ca6c1675574c04
WilliamFREAU <wfreau@gmail.com>
2018-12-01 00:48:40 +0100

    Form created

---
+++ src/main/java/UI/Utils/SortedDoubleList.java
@@ -0,0 +1,19 @@
+package UI.Utils;
+
+import javax.swing.*;
+
+public class SortedDoubleList<T> extends JPanel{
+    private JPanel root;
+    
+    private DoubleList<T> doubleList;
+    
+    private JButton rightListUp;
+    private JButton rightListDown;
+    private JButton leftListUp;
+    private JButton leftListDown;
+    
+    public SortedDoubleList () {
+        
+    }
+    
+}
