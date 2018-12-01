171c3ae90710927e39e746230df3cc650f18180a
WilliamFREAU <wfreau@gmail.com>
2018-12-01 16:48:12 +0100

    Ajout des tests sur la doubleList

--- src/test/java/UI/Utils/TestDoubleList.java
+++ src/test/java/UI/Utils/TestDoubleList.java
@@ -35,9 +35,28 @@ public class TestDoubleList {
         DoubleList<Object> doubleList = new DoubleList<Object>();
         
         doubleList.addToLeftList(obj1);
-        doubleList.addToRightList(obj1);
+        doubleList.addToRightList(obj2);
+        
+        doubleList.moveElementFromLeftToRight(obj1);
+        
+        assertEquals(0, doubleList.getLeftListModel().size());
+        assertEquals(2, doubleList.getRightListModel().size());
     }
     
+    @Test
+    public void testRightElementToLeft() {
+        Object obj1 = new Object();
+        Object obj2 = new Object();
     
+        DoubleList<Object> doubleList = new DoubleList<Object>();
+    
+        doubleList.addToLeftList(obj1);
+        doubleList.addToRightList(obj2);
+    
+        doubleList.moveElementFromRightToLeft(obj2);
+    
+        assertEquals(2, doubleList.getLeftListModel().size());
+        assertEquals(0, doubleList.getRightListModel().size());
+    }
     
 }
