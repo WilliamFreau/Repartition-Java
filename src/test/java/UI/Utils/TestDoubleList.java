2c3ab96c63465d104e1fa61090b70c08d42f1380
WilliamFREAU <wfreau@gmail.com>
2018-12-01 16:09:11 +0100

    Ajout de test sur doubleList

---
+++ src/test/java/UI/Utils/TestDoubleList.java
@@ -0,0 +1,15 @@
+package UI.Utils;
+
+public class TestDoubleList {
+    
+    
+    @Test
+    public void testLeftElementToRight() {
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+        
+        
+        
+    }
+    
+}
