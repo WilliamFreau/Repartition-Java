460bfae07d3033a4e7086afb3c4a4da104038ba3
WilliamFREAU <wfreau@gmail.com>
2018-12-01 01:13:19 +0100

    Ajout de la partie triage par bouton

--- src/main/java/UI/Utils/SortedDoubleList.java
+++ src/main/java/UI/Utils/SortedDoubleList.java
@@ -1,6 +1,7 @@
 package UI.Utils;
 
 import javax.swing.*;
+import java.util.List;
 
 public class SortedDoubleList<T> extends JPanel{
     private JPanel root;
@@ -22,26 +23,55 @@ public class SortedDoubleList<T> extends JPanel{
         initListener();
     }
     
-    
     private void initListener(){
-        this.leftListDown.addActionListener( event -> {
-            
+        this.leftListUp.addActionListener( event -> {
+            List<T> selected = this.doubleList.getLeftSelected();
+            this.moveUpSelected(selected, this.leftListModel);
         });
         
         this.leftListDown.addActionListener( event -> {
-            
+            List<T> selected = this.doubleList.getLeftSelected();
+            this.moveDownSelected(selected, this.leftListModel);
         });
         
         this.rightListUp.addActionListener( event -> {
-            
+            List<T> selected = this.doubleList.getRightSelected();
+            this.moveUpSelected(selected, this.rightListModel);
         });
         
         this.rightListDown.addActionListener( event -> {
-            
+            List<T> selected = this.doubleList.getLeftSelected();
+            this.moveDownSelected(selected, this.rightListModel);
         });
     }
-<<<<<<< HEAD
-=======
     
->>>>>>> origin/SortedDoubleList
+    private void moveUpSelected(List<T> selected, DefaultListModel<T> listModel)
+    {
+        for(T el : selected)
+        {
+            this.moveUp(el, listModel);
+        }
+    }
+    
+    private void moveDownSelected(List<T> selected, DefaultListModel<T> listModel)
+    {
+        for(T el : selected)
+        {
+            this.moveDown(el, listModel);
+        }
+    }
+    
+    private void moveUp(T element, DefaultListModel<T> listModel)
+    {
+        int currentIndex = listModel.indexOf(element);
+        listModel.remove(currentIndex);
+        listModel.insertElementAt(element, currentIndex-1);
+    }
+    
+    private void moveDown(T element, DefaultListModel<T> listModel)
+    {
+        int currentIndex = listModel.indexOf(element);
+        listModel.remove(currentIndex);
+        listModel.insertElementAt(element, currentIndex+1);
+    }
 }
