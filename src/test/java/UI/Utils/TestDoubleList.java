71b0be773f9b31e2cd29cec37a27253609720dbb
WilliamFREAU <wfreau@gmail.com>
2018-12-01 16:30:51 +0100

    Premier test écrits

--- src/test/java/UI/Utils/TestDoubleList.java
+++ src/test/java/UI/Utils/TestDoubleList.java
@@ -1,15 +1,43 @@
 package UI.Utils;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestDoubleList {
     
+    
+    @Test
+    public void testAddItemToLeftList() {
+        Object obj = new Object();
+        
+        DoubleList<Object> doubleList = new DoubleList<>();
+        doubleList.addToLeftList(obj);
+        
+        assertEquals(1, doubleList.getLeftListModel().size());
+    }
+    
+    @Test
+    public void testAddItemToRightList() {
+        Object obj = new Object();
+        
+        DoubleList<Object> doubleList = new DoubleList<>();
+        doubleList.addToRightList(obj);
+        
+        assertEquals(1, doubleList.getRightListModel().size());
+    }
     
     @Test
     public void testLeftElementToRight() {
         Object obj1 = new Object();
         Object obj2 = new Object();
         
+        DoubleList<Object> doubleList = new DoubleList<Object>();
         
-        
+        doubleList.addToLeftList(obj1);
+        doubleList.addToRightList(obj1);
     }
+    
+    
     
 }
