9d4ba2b64e69d0d20158fb41a469b61459084197
WilliamFREAU <wfreau@gmail.com>
2018-12-01 17:02:48 +0100

    Ajout de la classe de test de la SortedDoubleList

---
+++ src/test/java/UI/Utils/TestSortedDoubleList.java
@@ -0,0 +1,17 @@
+package UI.Utils;
+
+import org.junit.jupiter.api.Test;
+
+public class TestSortedDoubleList {
+    
+    @Test
+    public void moveUpLeftList () {
+        SortedDoubleList<Object> sorted = new SortedDoubleList<>();
+        
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+        
+        
+    }
+    
+}
